18e7de195219ec961d518173f8fb0d0604f6c24c
huochen <huochen@staff.sina.com.cn>
2017-07-27 11:12:43 +0800

    update logsize_exporter using prometheus_client

--- logsize_exporter.py
+++ logsize_exporter.py
@@ -1,52 +1,69 @@
-# encodings: utf-8
+# encoding: utf-8
+
+import sys
+import threading
+
+import prometheus_client
+
+from prometheus_client import Counter, Gauge
+from prometheus_client.core import CollectorRegistry
+from flask import Response, Flask
 
 from kazoo.client import KazooClient
 from kafka.consumer import KafkaConsumer
-from flask import Flask
- 
+
+
 app = Flask(__name__)
- 
-@app.route('/')
-def hello_world():
-    return 'Hello\n World'
- 
+
+REGISTRY = CollectorRegistry(auto_describe=False)
+
 zookeepers = "10.77.115.46:2181/kafka/sina1003"
 kafka_broker = "10.77.115.92"
+broker_list = kafka_broker.split(",")
 zk = KazooClient(hosts=zookeepers, read_only=True)
 zk.start()
+if not zk.exists("/brokers/topics"):
+    sys.exit(0)
 
-def output_format(topic, logsize, partition):
-    line = 'logsize{{topic="{0}",partition="{2}"}} {1}'.format(
-        topic,
-        logsize,
-        partition
-    )
-    return line
+
+kafka_logsize = Gauge("logsize",
+                      "Total count of kafka offset logsize",
+                      ["topic", "partition"],
+                      registry=REGISTRY)
+
+
+requests_total = Counter("request_count",
+                         "Total request count of the hosts",
+                         registry=REGISTRY)
+
+
+def thread_main(topic):
+    consumer = KafkaConsumer(topic,
+                             group_id='kafka_monitor',
+                             metadata_broker_list=broker_list)
+
+    offset = consumer._offsets.fetch
+
+    for part in offset:
+        kafka_logsize.labels(topic=part[0], partition=part[1]).set(
+            offset[part]
+        )
+
 
 @app.route("/metrics")
-def start():
-    html = ""
+def log_size():
+    # request_count + 1
+    requests_total.inc()
 
-    if zk.exists("/brokers/topics"):
-        topics = zk.get_children("/brokers/topics")
-        for i in range(len(topics)):
-            topic = topics[i]
+    topics = zk.get_children("/brokers/topics")
+    for i in range(len(topics)):
+        topic = topics[i]
+        t = threading.Thread(target=thread_main, args=(topic, ))
+        t.start()
 
-            broker_list = kafka_broker.split(",")
-            consumer = KafkaConsumer(topic,
-                                     group_id='kafka_monitor',
-                                     metadata_broker_list=broker_list)
-
-            offset = consumer._offsets.fetch
-            consumer.close()
-
-            for part in offset:
-                line = output_format(part[0], offset[part], part[1])
-                html += line + "\n"
-
-    return html
+    return Response(prometheus_client.generate_latest(REGISTRY),
+                    mimetype="text/plain")
 
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8082, threaded=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8082, threaded=True)
